d4e80591b106000ab506ae0af33b0ecf9c5acf31
solidpeat <solidpeat@gmail.com>
2016-12-17 21:53:48 +0900

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  context: __dirname + "/src",
+  entry: './index.js',
+  output: {
+    path: __dirname + "/dist",
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /(\.js|\.jsx)$/,
+        loader: 'babel!eslint',
+        exclude: /node_modules/
+      },
+      {
+        test: /(\.css|\.sass)$/,
+        loader: ExtractTextPlugin.extract(["css", "sass?includePaths[]=./node_modules"])
+      },
+      {
+        test: /\.pug$/,
+        loader: "pug?pretty=true"
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('[name].css'),
+    new HtmlWebpackPlugin({
+      template: "index.pug"
+    })
+  ],
+  devtool: 'inline-source-map',
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  }
+}
