8051af4e339aabed39919de7091bab4facd02360
solidpeat <solidpeat@gmail.com>
2016-12-23 14:49:44 +0900

    ProductとProductItemコンポーネント作成

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
+import ProductItem from './ProductItem';
+
+const product = {
+  title: 'masquerade',
+  price: 100,
+  inventory: 8,
+};
+const onAddToCartClicked = () => console.log('clicked');
 
 export default render(
-  <h1>Hello, World!</h1>,
+  <ProductItem
+    product={product}
+    onAddToCartClicked={onAddToCartClicked}
+  />,
   document.getElementById('root'),
 );
